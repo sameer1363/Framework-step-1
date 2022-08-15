c39044cbab82c22d716f7782ce97c51ab0bb3c2e
sameer zabiullah <sameerzabiullah@sameers-MBP.attlocal.net>
2022-08-15 13:51:48 -0700

    version1

---
+++ test/java/Steps/HomeStep.java
@@ -0,0 +1,47 @@
+package Steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomeStep {
+
+
+    @Given("user is navigated to url automationpractice application")
+    public void user_is_navigated_to_url_automationpractice_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click on sign in link button")
+    public void user_click_on_sign_in_link_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enter the email address in {string} section")
+    public void user_enter_the_email_address_in_section(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click on Create Account button")
+    public void user_click_on_create_account_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enter the Personal information ,Adderss and Contact info")
+    public void user_enter_the_personal_information_adderss_and_contact_info() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click on Register butoon")
+    public void user_click_on_register_butoon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Validate that user is created")
+    public void validate_that_user_is_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ test/resources/features/samtaxFramework.feature
@@ -0,0 +1,9 @@
+Feature:
+  Scenario:
+    Given user is navigated to url automationpractice application
+    When user click on sign in link button
+    And user enter the email address in ' Create and account ' section
+    And user click on Create Account button
+    And user enter the Personal information ,Adderss and Contact info
+    And user click on Register butoon
+    Then Validate that user is created
